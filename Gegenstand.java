3e631a660aafc80374f74f1b262dedafe825c4b4
tobias herhold <tobias02173@gmail.com>
2018-09-10 15:29:50 +0200

    welt, gegenstand hinzugefügt

---
+++ Gegenstand.java
@@ -0,0 +1,24 @@
+
+/**
+ * @author 
+ * @version 
+ */
+public class Gegenstand
+{
+    // Bezugsobjekte
+
+    // Attribute
+    private int xKoordinate;
+    private int yKoordinate;
+    private int schadenspunkte;
+    
+    // Konstruktor
+    public Gegenstand(int pXKoordinate,int pYKoordinate)
+    {
+        xKoordinate = pXKoordinate;
+        yKoordinate = pYKoordinate;
+    }
+
+    // Dienste
+
+}
---
+++ Welt.java
@@ -0,0 +1,23 @@
+
+/**
+ * @author 
+ * @version 
+ */
+public class Welt
+{
+    // Bezugsobjekte
+
+    // Attribute
+    private int xWert = 5;
+    private int yWert = 5;
+    private Gegenstand[][] weltArray;
+    
+    // Konstruktor
+    public Welt()
+    {
+         weltArray = new Gegenstand[xWert][yWert];
+    }
+
+    // Dienste
+
+}
